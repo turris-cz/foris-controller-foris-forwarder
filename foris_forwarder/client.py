8cc419fd1018c7971b57e0ea59a7ec3e081b9b5c
Stepan Henek <stepan.henek@nic.cz>
2020-08-12 18:42:26 +0200

    feature: client should be able to publish, subscribe and receive messages

--- foris_forwarder/client.py
+++ foris_forwarder/client.py
@@ -17,6 +17,7 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 #
 
+import json
 import logging
 import pathlib
 import threading
@@ -57,45 +58,82 @@ class PasswordSettings(Settings):
 
 
 class Client:
-    """ Class which handle connection to one message bus
+    """ Class which handle connection to one message bus (basically a wrapper arount MQTTClient)
 
         It listens to one mqtt bus and triggers a handler with incomming data
         It also uses a set of filter to filter messages which are sent as well
         as those which are received
     """
 
-    RETRY_CONNECT_TIMEOUT = 30.0
-    RETRY_CONNECT_INTERVAL = 0.5
     KEEPALIVE = 30
 
     def __init__(self, name: str, settings: Settings, listen_filters=[], send_filters=[]):
         self.name = name
         self.settings = settings
-        self.post_connect_hook: typing.Optional[typing.Callable[[mqtt.Client, dict, dict, int], None]] = None
-        self.post_disconnect_hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]] = None
+        self.connect_hook: typing.Optional[typing.Callable[[mqtt.Client, dict, dict, int], None]] = None
+        self.disconnect_hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]] = None
+        self.publish_hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]] = None
+        self.subscribe_hook: typing.Optional[
+            typing.Optional[typing.Callable[[mqtt.Client, dict, int, typing.List[int]], None]]
+        ] = None
+        self.message_hook: typing.Optional[
+            typing.Optional[typing.Callable[[mqtt.Client, dict, mqtt.MQTTMessage], None]]
+        ] = None
         self._connected = threading.Event()
         self.client: typing.Optional[mqtt.Client] = None
 
     def __str__(self):
         return f"{self.name}-forwarder"
 
+    def warning(self, template, *args, **kwargs):
+        logger.warning(f"%s: {template}", self, *args, **kwargs)
+
+    def debug(self, template, *args, **kwargs):
+        logger.debug(f"%s: {template}", self, *args, **kwargs)
+
+    def info(self, template, *args, **kwargs):
+        logger.info(f"%s: {template}", self, *args, **kwargs)
+
+    def error(self, template, *args, **kwargs):
+        logger.error(f"%s: {template}", self, *args, **kwargs)
+
+    def critical(self, template, *args, **kwargs):
+        logger.critical(f"%s: {template}", self, *args, **kwargs)
+
     def update(self, settings):
         raise NotImplementedError
 
-    def set_post_connect_hook(self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, dict, int], None]]):
-        self.post_connect_hook = hook
+    def set_connect_hook(self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, dict, int], None]]):
+        self.connect_hook = hook
 
-    def set_post_disconnect_hook(self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]]):
-        self.post_disconnect_hook = hook
+    def set_disconnect_hook(self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]]):
+        self.disconnect_hook = hook
+
+    def set_publish_hook(self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int], None]]):
+        self.publish_hook = hook
+
+    def set_subscribe_hook(
+        self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, int, typing.List[int]], None]],
+    ):
+        self.subscribe_hook = hook
+
+    def set_message_hook(
+        self, hook: typing.Optional[typing.Callable[[mqtt.Client, dict, mqtt.MQTTMessage], None]],
+    ):
+        self.message_hook = hook
 
     @property
     def connected(self):
-        return self._connected.is_set()
+        return bool(self.client) and self._connected.is_set()
+
+    def wait_until_connected(self, timeout: typing.Optional[float] = None):
+        """ blocks current thread until client is connected """
+        self._connected.wait(timeout)
 
     def connect(self):
 
         self.client = mqtt.Client(client_id=str(self), clean_session=False)
-        self.client.enable_logger()
+        self.client.enable_logger(logger)
 
         if self.settings.ca_certs and self.settings.certfile and self.settings.keyfile:
             self.client.tls_set(*map(str, (self.settings.ca_certs, self.settings.certfile, self.settings.keyfile)))
@@ -104,32 +142,87 @@ class Client:
             self.client.username_pw_set(self.settings.username, self.settings.password)
 
         def on_connect(client, userdata, flags, rc):
-            logger.debug(
+            self.debug(
                 "Forwarded %s is trying to connect to %s:%d", self, self.settings.host, self.settings.port,
             )
             if rc == 0:
-                logger.debug("Connected to %s - %s:%d", self, self.settings.host, self.settings.port)
+                self.debug("Connected to %s - %s:%d", self, self.settings.host, self.settings.port)
                 self._connected.set()
             else:
-                logger.warning("Failed to connect to %s - %s:%d", self, self.settings.host, self.settings.port)
+                self.warning("Failed to connect to %s - %s:%d", self, self.settings.host, self.settings.port)
 
-            if self.post_connect_hook:
-                self.post_connect_hook(client, userdata, flags, rc)
+            if self.connect_hook:
+                self.connect_hook(client, userdata, flags, rc)
 
         def on_disconnect(client, userdata, rc):
             if rc == 0:
-                logger.debug("Disconnected from %s - %s:%d", self, self.settings.host, self.settings.port)
+                self.debug("Disconnected from %s - %s:%d", self, self.settings.host, self.settings.port)
 
             self._connected.clear()
 
-            if self.post_disconnect_hook:
-                self.post_disconnect_hook(client, userdata, rc)
+            if self.disconnect_hook:
+                self.disconnect_hook(client, userdata, rc)
+
+        def on_publish(client, userdata, mid):
+            self.debug("Published (id=%d) was published", mid)
+            if self.publish_hook:
+                self.publish_hook(client, userdata, mid)
+
+        def on_subscribe(client, userdata, mid, granted_qos):
+            self.debug("Subscribed (mid=%d) was published")
+            if self.subscribe_hook:
+                self.subscribe_hook(client, userdata, mid, granted_qos)
+
+        def on_message(client, userdata, message: mqtt.MQTTMessage):
+            self.debug("Message Received (len=%s) for topic `%s`", len(message.payload), message.topic)
+            if self.message_hook:
+                self.message_hook(client, userdata, message)
 
         self.client.on_connect = on_connect
         self.client.on_disconnect = on_disconnect
+        self.client.on_publish = on_publish
+        self.client.on_subscribe = on_subscribe
+        self.client.on_message = on_message
         self.client.connect_async(self.settings.host, self.settings.port, Client.KEEPALIVE)
 
         self.client.loop_start()
+
+    def publish(self, topic: str, data: typing.Optional[dict]) -> typing.Optional[int]:
+        """ Publishes messages
+
+        This doesn't mean that the message was acutally sent.
+        on_publish hook should be checked to determined whether the message was sent
+        """
+        if self.connected and self.client is not None:
+            message = self.client.publish(topic, json.dumps(data, separators=(",", ":")))
+            # this doesn't mean that the message was publish (on_publish callback)
+            if message.rc == mqtt.MQTT_ERR_SUCCESS:
+                self.debug("Publishing message to '%s' (mid=%d)", topic, message.mid)
+                return message.mid
+            else:
+                self.warning("Failed to publish message to '%s'", topic)
+                return None
+        else:
+            self.warning("Disconnected, can't send message to '%s'", topic)
+            return None
+
+    def subscribe(self, topics: typing.List[typing.Tuple[str, int]]) -> bool:
+        """ Subscibes to a topic
+
+        This doesn't mean the client is subscribed for given topic.
+        on_subscribe hook should be checked to determined whether the topic was subscribed
+        """
+        if self.connected and self.client is not None:
+            (res, mid) = self.client.subscribe(topics)
+            if res == mqtt.MQTT_ERR_SUCCESS:
+                self.debug("Subscribed to '%s'", topics)
+                return True
+            else:
+                self.warning("Failed to subscribe to '%s'", topics)
+                return False
+        else:
+            self.warning("Disconnected, failed to subscribe to '%s'", topics)
+            return False
 
     def disconnect(self):
         self.client.loop_stop()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,51 +1,99 @@
 import threading
 
-from foris_forwarder.client import CertificateSettings, Client, PasswordSettings
+from foris_forwarder.client import Client
+
+TIMEOUT = 30.0
 
 
 def test_connection(prepare_ca, connection_settings):
     process, settings = connection_settings
-    client = Client("certificate-test", settings)
+    client = Client("connection-test", settings)
 
     assert client.connected is False
 
     # add post_connect hook
     connect_event = threading.Event()
 
-    def post_connect(client, userdata, flags, rc):
+    def connect(client, userdata, flags, rc):
         connect_event.set()
 
-    client.set_post_connect_hook(post_connect)
+    client.set_connect_hook(connect)
 
     # add post_disconnect hook
     disconnect_event = threading.Event()
 
-    def post_disconnect(client, userdata, rc):
+    def disconnect(client, userdata, rc):
         disconnect_event.set()
 
-    client.set_post_disconnect_hook(post_disconnect)
+    client.set_disconnect_hook(disconnect)
 
     # test connect
     client.connect()
 
-    assert connect_event.wait(30.0)
+    assert connect_event.wait(TIMEOUT)
     assert client.connected is True
 
     # test disconnect
     client.disconnect()
 
-    assert disconnect_event.wait(30.0)
+    assert disconnect_event.wait(TIMEOUT)
     assert client.connected is False
 
     # terminte message bus
     connect_event.clear()
     client.connect()
 
-    assert connect_event.wait(30.0)
+    assert connect_event.wait(TIMEOUT)
     assert client.connected is True
 
+    disconnect_event.clear()
     process.kill()
 
-    disconnect_event.clear()
-    assert disconnect_event.wait(30.0)
+    assert disconnect_event.wait(TIMEOUT)
     assert client.connected is False
+
+
+def test_messaging(prepare_ca, connection_settings):
+    process, settings = connection_settings
+
+    # prepare listener
+    client_listener = Client("listener", settings)
+    subscribe_event = threading.Event()
+
+    def subscribe(client, userdata, mid, granted_qos):
+        subscribe_event.set()
+
+    client_listener.set_subscribe_hook(subscribe)
+
+    message_event = threading.Event()
+
+    def message(client, userdata, message):
+        message_event.set()
+
+    client_listener.set_message_hook(message)
+
+    # prepare publisher
+    client_publisher = Client("publisher", settings)
+    publish_event = threading.Event()
+
+    def publish(client, userdata, mid):
+        publish_event.set()
+
+    client_publisher.set_publish_hook(publish)
+
+    # wait till listener is subscribed
+    client_listener.connect()
+    client_listener.wait_until_connected(TIMEOUT)
+    client_publisher.connect()
+    client_publisher.wait_until_connected(TIMEOUT)
+
+    # wait till listener is subscribed for topic
+    assert client_listener.subscribe([("/messaging-test/+", 0)])
+    assert subscribe_event.wait(TIMEOUT)
+
+    # wait till publisher publishes a message
+    assert client_publisher.publish("/messaging-test/first", {"some": "data"}) is not None
+    assert publish_event.wait(TIMEOUT)
+
+    # check that the message was obtained by the listener
+    assert message_event.wait(TIMEOUT)
